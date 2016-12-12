a51051982ceaf986310578d9805f73cf4a56e277
nguyen.thai.phuong@framgia.com <nguyen.thai.phuong@framgia.com>
2016-12-12 15:38:02 +0700

    thu tu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,10 +40,10 @@ Rails.application.configure do
   #   :enable_starttls_auto => true
   # }
 
-  host = 'https://radiant-beyond-60455.herokuapp.com'
+  host = 'radiant-beyond-60455.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
